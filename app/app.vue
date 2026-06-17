76a669c7829e07d94feedcc600730ca119fb2bb0
bikathi <bikathimartin@aol.com>
2026-06-17 11:39:05 +0300

    First commit - Setup Nuxt, Tailwind and FlyonUI

---
+++ app/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="bg-red-500 text-xl">Hello worl</h1>
+    <button class="btn">Default</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-info">Info</button>
+    <button class="btn btn-success">Success</button>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-error">Error</button>
+  </div>
+
+  <div>
+    <button class="btn btn-outline">Default</button>
+    <button class="btn btn-outline btn-primary">Primary</button>
+    <button class="btn btn-outline btn-secondary">Secondary</button>
+    <button class="btn btn-outline btn-accent">Accent</button>
+    <button class="btn btn-outline btn-info">Info</button>
+    <button class="btn btn-outline btn-success">Success</button>
+    <button class="btn btn-outline btn-warning">Warning</button>
+    <button class="btn btn-outline btn-error">Error</button>
+  </div>
+</template>
---
+++ app/plugins/flyonui.client.ts
@@ -0,0 +1,10 @@
+import { useRouter } from "vue-router";
+
+import "flyonui/dist/accordion";
+
+export default defineNuxtPlugin(() => {
+//   const router = useRouter();
+//   router.afterEach(async () => {
+//     setTimeout(() => window.HSAccordion.autoInit());
+//   });
+});
